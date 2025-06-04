e3015f51f2f936d0076df2ce6f93c2b587c4a1f2
ksks2012 <st890751999@gmail.com>
2025-06-04 12:29:29 +0800

    feat(lissajous): Enhance color palette and update drawing logic for Lissajous figures

--- ch1/lissajousp/main.go
+++ ch1/lissajousp/main.go
@@ -3,6 +3,8 @@
 
 // Run with "web" command-line argument for web server.
 // See page 13.
+// Practice 1.5: Change color black to green.
+// Practice 1.6: More colors in Lissajous figures.
 //!+main
 
 // Lissajous generates GIF animations of random Lissajous figures.
@@ -26,31 +28,30 @@ import (
 
 //!+main
 
-var palette = []color.Color{color.White, color.RGBA{0x00, 0xff, 0x00, 0xff}}
-
-const (
-	whiteIndex = 0 // first color in palette
-	blackIndex = 1 // next color in palette
-)
+var palette = []color.Color{
+	color.White,                        // 0: white background
+	color.RGBA{0x00, 0xff, 0x00, 0xff}, // 1: bright green
+	color.RGBA{0x00, 0xcc, 0x00, 0xff}, // 2: darker green
+	color.RGBA{0x00, 0x99, 0x00, 0xff}, // 3: even darker green
+	color.RGBA{0x00, 0x00, 0xff, 0xff}, // 4: bright blue
+	color.RGBA{0x00, 0x00, 0xcc, 0xff}, // 5: darker blue
+	color.RGBA{0x00, 0x00, 0x99, 0xff}, // 6: even darker blue
+	color.RGBA{0x80, 0x00, 0x80, 0xff}, // 7: purple
+	color.RGBA{0xff, 0xa5, 0x00, 0xff}, // 8: orange
+	color.RGBA{0xff, 0x00, 0x00, 0xff}, // 9: red
+}
 
 func main() {
-	//!-main
-	// The sequence of images is deterministic unless we seed
-	// the pseudo-random number generator using the current time.
-	// Thanks to Randall McPherson for pointing out the omission.
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	if len(os.Args) > 1 && os.Args[1] == "web" {
-		//!+http
 		handler := func(w http.ResponseWriter, r *http.Request) {
 			lissajous(w)
 		}
 		http.HandleFunc("/", handler)
-		//!-http
 		log.Fatal(http.ListenAndServe("localhost:8000", nil))
 		return
 	}
-	//!+main
 	lissajous(os.Stdout)
 }
 
@@ -65,14 +66,25 @@ func lissajous(out io.Writer) {
 	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0 // phase difference
+
+	numDrawingColors := len(palette) - 1
+
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
+
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
+
+			// Select color index based on the value of 't'
+			// Map t to a value between 1 and numDrawingColors
+			// (t / (cycles * 2 * math.Pi)) gets the proportion of t in the whole cycle (0.0 to 1.0)
+			// Multiply by numDrawingColors and add 1 to get the corresponding color index
+			colorIndex := uint8(1 + int(t/(cycles*2*math.Pi)*float64(numDrawingColors))%numDrawingColors)
+
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				blackIndex)
+				colorIndex)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
