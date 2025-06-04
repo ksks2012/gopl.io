0f372590edadba56ac2f084e2f5cf2dfa50ae103
ksks2012 <st890751999@gmail.com>
2025-06-04 16:18:38 +0800

    feat(p1.7): Implement fetch functionality to retrieve and print content from specified URLs

---
+++ ch1/fetchp/main.go
@@ -0,0 +1,36 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 16.
+//!+
+
+// Fetch prints the content found at each specified URL.
+// Practice 1.7: Replace ioutil.ReadAll() by using io.Copy(dst, src)
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+
+		out := os.Stdout // or any other io.Writer
+		_, err = io.Copy(out, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
+
+//!-
