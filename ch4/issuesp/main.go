5171cd6d630801e47269ebd71e732f75329cfb32
ksks2012 <st890751999@gmail.com>
2025-06-08 15:15:04 +0800

    feat(issues): Implement grouping of GitHub issues by age in the output

---
+++ ch4/issuesp/main.go
@@ -0,0 +1,75 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 112.
+//!+
+
+// Issues prints a table of GitHub issues matching the search terms.
+// Practice 4.10: Modify the issues program to print the issues in groups by age: less than 30 days, 30 to 365 days, and more than 365 days old.
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gopl.io/ch4/github"
+)
+
+// !+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d issues:\n", result.TotalCount)
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d %9.9s %.55s\n",
+			item.Number, item.User.Login, item.Title)
+	}
+
+	mp := make(map[string][]*github.Issue)
+	now := time.Now()
+	for _, item := range result.Items {
+		days := int(now.Sub(item.CreatedAt).Hours() / 24)
+		if days < 30 {
+			mp["<30 days"] = append(mp["<30 days"], item)
+		} else if days <= 365 {
+			mp["30-365 days"] = append(mp["30-365 days"], item)
+		} else {
+			mp[">365 days"] = append(mp[">365 days"], item)
+		}
+	}
+
+	for k, issues := range mp {
+		fmt.Printf("\n%s:\n", k)
+		for _, item := range issues {
+			fmt.Printf("#%-5d %9.9s %.55s\n",
+				item.Number, item.User.Login, item.Title)
+		}
+	}
+}
+
+//!-
+
+/*
+//!+textoutput
+$ go build gopl.io/ch4/issues
+$ ./issues repo:golang/go is:open json decoder
+13 issues:
+#5680    eaigner encoding/json: set key converter on en/decoder
+#6050  gopherbot encoding/json: provide tokenizer
+#8658  gopherbot encoding/json: use bufio
+#8462  kortschak encoding/json: UnmarshalText confuses json.Unmarshal
+#5901        rsc encoding/json: allow override type marshaling
+#9812  klauspost encoding/json: string tag not symmetric
+#7872  extempora encoding/json: Encoder internally buffers full output
+#9650    cespare encoding/json: Decoding gives errPhase when unmarshalin
+#6716  gopherbot encoding/json: include field name in unmarshal error me
+#6901  lukescott encoding/json, encoding/xml: option to treat unknown fi
+#6384    joeshaw encoding/json: encode precise floating point integers u
+#6647    btracey x/tools/cmd/godoc: display type kind of each named type
+#4237  gjemiller encoding/base64: URLEncoding padding is optional
+//!-textoutput
+*/
