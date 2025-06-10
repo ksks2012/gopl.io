05d19b78982e653a71124ab448539c0d7dcf0dc4
ksks2012 <st890751999@gmail.com>
2025-06-10 18:06:23 +0800

    feat(findlinks): Implement findlinks tool to extract links from HTML documents

---
+++ ch5/findlinks1p/main.go
@@ -0,0 +1,95 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 122.
+//!+main
+
+// Findlinks1 prints the links in an HTML document read from standard input.
+// Practice 5.1: Recursively visit the children of a node, using NextSibling and FirstChild.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	filepath := os.Args[1]
+	if filepath == "" {
+		fmt.Fprintln(os.Stderr, "Usage: findlinks1 <file>")
+		os.Exit(1)
+	}
+	htmlFile, err := os.Open(filepath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	defer htmlFile.Close()
+
+	doc, err := html.Parse(htmlFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+//!-main
+
+// !+visit
+// visit appends to links each link found in n and returns the result.
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+
+	if n.FirstChild != nil {
+		links = visit(links, n.FirstChild)
+	}
+
+	if n.NextSibling != nil {
+		links = visit(links, n.NextSibling)
+	}
+
+	return links
+}
+
+//!-visit
+
+/*
+//!+html
+package html
+
+type Node struct {
+	Type                    NodeType
+	Data                    string
+	Attr                    []Attribute
+	FirstChild, NextSibling *Node
+}
+
+type NodeType int32
+
+const (
+	ErrorNode NodeType = iota
+	TextNode
+	DocumentNode
+	ElementNode
+	CommentNode
+	DoctypeNode
+)
+
+type Attribute struct {
+	Key, Val string
+}
+
+func Parse(r io.Reader) (*Node, error)
+//!-html
+*/
