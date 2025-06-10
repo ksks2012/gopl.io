7ae2ff2191822e32f2682b49fcde3088f6eb65d4
ksks2012 <st890751999@gmail.com>
2025-06-10 22:05:39 +0800

    feat(findlinks): Add functionality to count words and images in HTML documents

---
+++ ch5/findlinks2p/main.go
@@ -0,0 +1,60 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// Practice 5.5: Count the number of words and images in an HTML document
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <url>\n", os.Args[0])
+		os.Exit(1)
+	}
+	url := os.Args[1]
+
+	words, images, err := CountWordsAndImages(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error counting words and images: %v\n", err)
+		return
+	}
+	fmt.Printf("Words: %d, Images: %d\n", words, images)
+}
+
+// CountWordsAndImages does an HTTP GET request for the HTML
+// document url and returns the number of words and images in it.
+func CountWordsAndImages(url string) (words, images int, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		err = fmt.Errorf("parsing HTML: %s", err)
+		return
+	}
+	words, images = countWordsAndImages(doc)
+	return
+}
+func countWordsAndImages(n *html.Node) (words, images int) {
+	if n.Type == html.TextNode {
+		fields := strings.Fields(n.Data)
+		words += len(fields)
+	} else if n.Type == html.ElementNode && n.Data == "img" {
+		images++ // Count images in img elements
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		w, i := countWordsAndImages(c)
+		words += w
+		images += i
+	}
+	return
+}
