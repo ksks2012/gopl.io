d02bd4be56b7e82778429ad78bdbaa9d956ca4c6
ksks2012 <st890751999@gmail.com>
2025-06-21 10:14:12 +0800

    refactor(cake_test): Remove outdated comments and set Verbose in benchmarks

--- ch8/cakep/cake_test.go
+++ ch8/cakep/cake_test.go
@@ -1,6 +1,3 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
 package cake_test
 
 import (
@@ -11,7 +8,6 @@ import (
 )
 
 var defaults = cake.Shop{
-	Verbose:      testing.Verbose(),
 	Cakes:        20,
 	BakeTime:     10 * time.Millisecond,
 	NumIcers:     1,
@@ -20,55 +16,50 @@ var defaults = cake.Shop{
 }
 
 func Benchmark(b *testing.B) {
-	// Baseline: one baker, one icer, one inscriber.
-	// Each step takes exactly 10ms.  No buffers.
 	cakeshop := defaults
-	cakeshop.Work(b.N) // 224 ms
+	cakeshop.Verbose = testing.Verbose()
+	cakeshop.Work(b.N)
 }
 
 func BenchmarkBuffers(b *testing.B) {
-	// Adding buffers has no effect.
 	cakeshop := defaults
+	cakeshop.Verbose = testing.Verbose()
 	cakeshop.BakeBuf = 10
 	cakeshop.IceBuf = 10
-	cakeshop.Work(b.N) // 224 ms
+	cakeshop.Work(b.N)
 }
 
 func BenchmarkVariable(b *testing.B) {
-	// Adding variability to rate of each step
-	// increases total time due to channel delays.
 	cakeshop := defaults
+	cakeshop.Verbose = testing.Verbose()
 	cakeshop.BakeStdDev = cakeshop.BakeTime / 4
 	cakeshop.IceStdDev = cakeshop.IceTime / 4
 	cakeshop.InscribeStdDev = cakeshop.InscribeTime / 4
-	cakeshop.Work(b.N) // 259 ms
+	cakeshop.Work(b.N)
 }
 
 func BenchmarkVariableBuffers(b *testing.B) {
-	// Adding channel buffers reduces
-	// delays resulting from variability.
 	cakeshop := defaults
+	cakeshop.Verbose = testing.Verbose()
 	cakeshop.BakeStdDev = cakeshop.BakeTime / 4
 	cakeshop.IceStdDev = cakeshop.IceTime / 4
 	cakeshop.InscribeStdDev = cakeshop.InscribeTime / 4
 	cakeshop.BakeBuf = 10
 	cakeshop.IceBuf = 10
-	cakeshop.Work(b.N) // 244 ms
+	cakeshop.Work(b.N)
 }
 
 func BenchmarkSlowIcing(b *testing.B) {
-	// Making the middle stage slower
-	// adds directly to the critical path.
 	cakeshop := defaults
+	cakeshop.Verbose = testing.Verbose()
 	cakeshop.IceTime = 50 * time.Millisecond
-	cakeshop.Work(b.N) // 1.032 s
+	cakeshop.Work(b.N)
 }
 
 func BenchmarkSlowIcingManyIcers(b *testing.B) {
-	// Adding more icing cooks reduces the cost of icing
-	// to its sequential component, following Amdahl's Law.
 	cakeshop := defaults
+	cakeshop.Verbose = testing.Verbose()
 	cakeshop.IceTime = 50 * time.Millisecond
 	cakeshop.NumIcers = 5
-	cakeshop.Work(b.N) // 288ms
+	cakeshop.Work(b.N)
 }
